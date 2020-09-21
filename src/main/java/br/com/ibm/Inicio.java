b6ce5bfa6d679cbeedf7a579e25ced17a242899d
apwaldman <apwaldman@gmail.com>
2020-09-20 23:28:01 -0300

    ajuste na mensagem de execução do sistema

--- src/main/java/br/com/ibm/Inicio.java
+++ src/main/java/br/com/ibm/Inicio.java
@@ -17,11 +17,11 @@ public class Inicio {
 		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 		Date date = new Date();
 		System.out.println(dateFormat.format(date)); // 2016/11/16 12:08:43
-
-		if (date.getHours() >= 9 && date.getHours() <= 10) {
-			CriaCSVRetorno criaCSV = new CriaCSVRetorno();
+		CriaCSVRetorno criaCSV = new CriaCSVRetorno();
+		if (date.getHours() >= 9 && date.getHours() <= 10) {			
 			criaCSV.CriarCVS();
-
+		}else {
+			System.out.println("O sistema está configurado para ser executado entre as 9 e 10h. Para executar fora desse horário, ajuste os parâmetros");			
 		}
 	}
 }
